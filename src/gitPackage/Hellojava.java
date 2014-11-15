71728c5fadda15cfb05a1876a4f8083975665d81
amirb100 <amir.betser@gmail.com>
2014-11-15 18:00:31 +0200

    Initial

---
+++ src/gitPackage/Hellojava.java
@@ -0,0 +1,14 @@
+package gitPackage;
+
+public class Hellojava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java - Initial");
+
+	}
+
+}
